2956928dfe3a8462398fa26649249413105dc422
jdtrades <65446494+jdtrades@users.noreply.github.com>
2026-09-16 12:26:25 -0500

    Add Stanza app shell and co-writer panel

---
+++ src/lib/utils.ts
@@ -0,0 +1,19 @@
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function uid() {
+  return crypto.randomUUID();
+}
+
+export function formatTime(ts: number) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "2-digit",
+  }).format(ts);
+}
